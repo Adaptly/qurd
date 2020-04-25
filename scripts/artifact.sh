6c095dd151a053b73cc85252ff9a54bd74ce6502
bpilewski <bpilewski@adaptly.com>
2020-04-24 21:33:02 -0400

    move this here from jenkins config

--- scripts/artifact.sh
+++ scripts/artifact.sh
@@ -2,6 +2,7 @@
 set -e
 bundle package --all
 bundle install --binstubs bundler-bin --deployment --local --without development test
+AWS_PROFILE=zoo bundle exec rake minitest || :
 echo ${BUILD_NUMBER} > .release.txt
 git rev-parse --verify HEAD >> .release.txt
 rm -rf release
